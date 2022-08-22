8b6fa3d1532a298407f56b08ee60e61648049bb4
Филатов Руслан Викторович <@lcoast.itstep>
2022-08-22 17:16:28 +0600

    Initial commit

---
+++ src/Structural/Adapter/LineAdapter.java
@@ -0,0 +1,14 @@
+package Structural.Adapter;
+
+public class LineAdapter implements Shape {
+    private Line adapter;
+
+    public LineAdapter(Line adapter){
+        this.adapter = adapter;
+    }
+
+    @Override
+    public void draw(int x, int y, int z, int j){
+        adapter.draw(x,y,z,j);
+    }
+}
---
+++ src/Structural/Adapter/Rectangle.java
@@ -0,0 +1,7 @@
+package Structural.Adapter;
+
+public class Rectangle {
+    public void draw(int x, int y, int width, int height){
+        System.out.println("Rectangle with coordinate left-down point (" + x + ";" + y + "");
+    }
+}
